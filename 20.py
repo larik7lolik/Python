8664e73c06ef3504e1648bd254698450ab5c47ff
LarisaZaripova <larik7lolik@gmail.com>
2022-05-13 13:55:36 +0300

    added task_20.py

---
+++ 20.py
@@ -0,0 +1,21 @@
+# Определить, присутствует ли в заданном списке строк, некоторое число 
+from cgitb import text
+import os
+os.system("cls")
+
+list = ['red', 7, 'green', 4,'blue']
+#print(type(list))
+#list = int
+print(list)
+
+i = 0
+while i==int():
+    print('True')
+    break
+else:
+    print('False')
+
+
+
+
+
